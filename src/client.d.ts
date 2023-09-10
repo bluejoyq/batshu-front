c0b05e5793560c028eb2ce2f09ea99a51c987a4f
bluejoyq <bluejoy32@gmail.com>
2023-09-10 19:27:43 +0900

    feat: add icons
    Co-authored-by: HYEOK9 <HYEOK9@users.noreply.github.com>

--- src/client.d.ts
+++ src/client.d.ts
@@ -1,5 +1,7 @@
 interface ImportMetaEnv {
-  readonly VITE_EXAMPLE: string;
+  readonly VITE_KAKAO_REST_KEY: string;
+  readonly VITE_REST_API_KEY: string;
+  readonly VITE_KAKAO_REST_API_KEY: string;
   // 다른 환경 변수들에 대한 타입 정의...
 }
 
